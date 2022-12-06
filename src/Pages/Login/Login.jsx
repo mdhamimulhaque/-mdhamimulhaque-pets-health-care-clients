b9616724f16bed10ee51a7c7242c2a2cf86d9488
Md Hamimul Haque <mdhamimulhaque@gmail.com>
2022-12-06 22:43:27 +0600

    test

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -19,7 +19,6 @@ const Login = () => {
     // ---> provider
     const googleProvider = new GoogleAuthProvider();
 
-
     // ---> handle google login
     const handleGoogleLogIn = () => {
         googleLogIn(googleProvider)
